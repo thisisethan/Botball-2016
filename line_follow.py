d49ee78d6b8216ba400582e550b2a5489a1475c6
Ethan Rankin <jesuisethan@gmail.com>
2016-03-29 13:22:05 -0400

    Updated for docs to show change

--- line_follow.py
+++ line_follow.py
@@ -5,11 +5,13 @@ assumes that we start on the black line when function is called
 '''
 
 BLACK_THRESHOLD = 3000;
+"""value given when the IR sensor is over a black area"""
 
 from movement import *
 from decorators import timeout,TimeoutError
 from wallaby import analog
 IR_SENSOR_PORT = 0;
+"""sensor port in which the IR sensor is located"""
 
 def line_follow():
     while True:
@@ -20,7 +22,7 @@ def line_follow():
 
         if (analog(IR_SENSOR_PORT) < BLACK_THRESHOLD):
             turn_right_for_time(100,100)
-
+    """robot follows the left edge of a black line on the white board"""
 
 
 
@@ -28,5 +30,9 @@ def line_follow():
 def line_follow_time(time):
     try:
         timeout(time)(line_follow)()
+            return 1;
     except TimeoutError:
         break
+    """executes the line follow function for a given time period"""
+    """takes a variable time, which is the duration the robot is to line follow"""
+    """returns one if the function runs successfully"""
